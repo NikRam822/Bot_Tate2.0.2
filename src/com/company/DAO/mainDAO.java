9f3ed7c39051d3a623f9793c0bf5465fb02d4706
Ilya_Bat9 <nasirov@olympus.ru>
2021-09-30 00:36:01 +0500

    adding sqlite and sqliteorm

---
+++ src/com/company/DAO/mainDAO.java
@@ -0,0 +1,10 @@
+package com.company.DAO;
+
+public class mainDAO {
+
+    Dao<Account, String> accountDao =
+            DaoManager.createDao(connectionSource, Account.class);
+    Dao<Order, Integer> orderDao =
+            DaoManager.createDao(connectionSource, Order.class);
+
+}
--- src/com/company/main/Main.java
+++ src/com/company/main/Main.java
@@ -1,12 +1,23 @@
 package com.company.main;
 
 import com.company.telegram.TelegramBot;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.*;
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
@@ -16,6 +27,59 @@ public class Main {
 
         } catch (TelegramApiRequestException e){
             e.printStackTrace();
+        }
+
+        // this uses h2 by default but change to match your database
+        String databaseUrl = "jdbc:sqlite::memory:";
+        // create a connection source to our database
+        ConnectionSource connectionSource =
+                new JdbcConnectionSource(databaseUrl);
+
+        // instantiate the dao
+        Dao<Account, String> accountDao =
+                DaoManager.createDao(connectionSource, Account.class);
+
+        // if you need to create the 'accounts' table make this call
+        TableUtils.createTable(connectionSource, Account.class);
+
+        Connection connection = null;
+        try
+        {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite::memory:");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            statement.executeUpdate("drop table if exists person");
+            statement.executeUpdate("create table person (id integer, name string)");
+            statement.executeUpdate("insert into person values(1, 'leo')");
+            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from person");
+            while(rs.next())
+            {
+                // read the result set
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        }
+        catch(SQLException e)
+        {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if(connection != null)
+                    connection.close();
+            }
+            catch(SQLException e)
+            {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
         }
     }
 }
