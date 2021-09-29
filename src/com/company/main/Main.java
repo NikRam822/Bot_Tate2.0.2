b4de0e25c68858def999803733bef2217784d4d0
Feldjer <sahrat.r@mail.ru>
2021-09-29 13:29:02 +0500

    Добавил скелет для тг

--- src/com/company/main/Main.java
+++ src/com/company/main/Main.java
@@ -1,7 +1,21 @@
 package com.company.main;
 
+import com.company.telegram.TelegramBot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("12");
+
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try{
+            TelegramBot bot = new TelegramBot();
+            telegramBotsApi.registerBot(bot);
+
+        } catch (TelegramApiRequestException e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/com/company/telegram/TelegramBot.java
@@ -0,0 +1,45 @@
+package com.company.telegram;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    public void sendMsg(Message message, String text) {
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void onUpdateReceived(Update update) {//метод для приема сообщений
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                String command = message.getText();
+                //sendMsg(message,"kjxxxxxx");
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "tote_project_bot";
+    }
+
+    public String getBotToken() {
+        return "1649189668:AAGDg8CYBi7FRQfzK34zWtAQub_WlsKK2Z4";
+    }
+}
+
